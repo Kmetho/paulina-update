e068072885c774a2290720f4f063430357eb9054
Wercche <lavellanc@gmail.com>
2026-06-17 10:36:19 +0200

    migrate to Astro

---
+++ public/scripts/custom.js
@@ -0,0 +1,11 @@
+$(function () {
+  try {
+    $("body").ripples({
+      resolution: 512,
+      dropRadius: 20,
+      perturbance: 0.08,
+    });
+  } catch (e) {
+    $(".error").show().text(e);
+  }
+});
